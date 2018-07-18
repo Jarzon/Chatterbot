6fc7ca1cf2bf7022c88d09d0c52ac0f6e05061cc
Jarzon <j@jasonvaillancourt.ca>
2018-07-18 01:18:23 -0400

    Working chatterbot

--- src/BasePack/Controller/Home.php
+++ src/BasePack/Controller/Home.php
@@ -5,18 +5,23 @@ use Prim\Controller;
 
 class Home extends Controller
 {
-    public function index()
-    {
-        $this->addVar('name', 'anonymous');
+    /**
+     * @var \Chatterbot\ChatterbotPack\Service\SentenceHelper $sentenceHelper
+     * @var \Chatterbot\ChatterbotPack\Model\SentenceModel $model
+     */
+    public $sentenceHelper;
+    public $model;
 
-        $this->render('home/index');
+    function build() {
+        $this->sentenceHelper = $this->container->getSentenceHelper();
     }
 
     public function discord()
     {
+        $this->model = $this->getModel('SentenceModel', 'ChatterbotPack');
+
         $discord = new \Discord\Discord([
-            'token' => 'NDY4NTEyNTkzNzA4MDU2NTk2.DjApyA.TRct1SRGcuzxyRtfuTI9qnViUP4',
-            'name' => 'Botty'
+            'token' => $this->options['discord_token']
         ]);
 
         $discord->on('ready', function ($discord) {
@@ -24,7 +29,17 @@ class Home extends Controller
 
             // Listen for events here
             $discord->on('message', function ($message) {
-                echo "Recieved a message from {$message->author->username}: {$message->content}", PHP_EOL;
+                if($message->author->id !== '468512593708056596' && strpos($message->content, "@{$this->options['discord_bot_name']}")) {
+                    echo "Recieved a message from {$message->author->username}: {$message->content}", PHP_EOL;
+
+                    $words = $this->sentenceHelper->getWords($message->content);
+
+                    $response = $this->model->getResponse($words);
+
+                    if($response) {
+                        $message->channel->sendMessage($response[0]->sentence);
+                    }
+                }
             });
         });
 
